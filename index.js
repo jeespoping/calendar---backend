55061266b01793cc520eb9f4aa280b3a9e373f16
meanuel <manuellopezflorez134@gmail.com>
2021-08-09 17:33:46 -0500

    creacion de backend en login

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+require('dotenv').config();
+
+//crear el servidor de express
+const app = express();
+
+// base de datos
+dbConnection();
+
+//CORS
+app.use(cors())
+
+//directorio publico
+app.use(express.static('public'));
+
+// Leactura y parseo del body
+app.use(express.json());
+
+//Rutas
+app.use('/api/auth', require('./routes/auth'));
+
+
+app.listen(process.env.PORT, () => {
+   console.log(`Servidor en puerto ${process.env.PORT}`); 
+});
