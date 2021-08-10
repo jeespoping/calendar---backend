ed5678f76320c033e2dc862599ac7c2d6796a5e0
meanuel <manuellopezflorez134@gmail.com>
2021-08-10 11:19:51 -0500

    aregando el populate

--- controllers/events.js
+++ controllers/events.js
@@ -3,7 +3,7 @@ const Evento = require('../models/Evento');
 
 const getEventos = async( req, res = response ) => {
 
-    const eventos = await Evento.find();
+    const eventos = await Evento.find().populate('user','name');
 
     res.json({
         ok: true,
